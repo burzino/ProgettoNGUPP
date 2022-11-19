c842374f801ec55d27f0a58172ceedfdbdb1a2b9
burzino <44402137+burzino@users.noreply.github.com>
2022-11-19 10:52:59 +0100

    update

--- README.md
+++ README.md
@@ -5,3 +5,5 @@
 ### 2- sito web suddiviso in front end e back end
 ### > Per avviare il front end installare angular e nella directory WEBSITE su cmd usare il comando ng serve -o
 ### > Per avviare il backend installare node e nella directory BACK su cmd eseguire il comando node index.js
+
+
--- WEBSITE/src/app/layouts/admin-layout/admin-layout.routing.ts
+++ WEBSITE/src/app/layouts/admin-layout/admin-layout.routing.ts
@@ -10,51 +10,13 @@ import { NotificationsComponent } from '../../notifications/notifications.compon
 import { UpgradeComponent } from '../../upgrade/upgrade.component';
 
 export const AdminLayoutRoutes: Routes = [
-    // {
-    //   path: '',
-    //   children: [ {
-    //     path: 'dashboard',
-    //     component: DashboardComponent
-    // }]}, {
-    // path: '',
-    // children: [ {
-    //   path: 'userprofile',
-    //   component: UserProfileComponent
-    // }]
-    // }, {
-    //   path: '',
-    //   children: [ {
-    //     path: 'icons',
-    //     component: IconsComponent
-    //     }]
-    // }, {
-    //     path: '',
-    //     children: [ {
-    //         path: 'notifications',
-    //         component: NotificationsComponent
-    //     }]
-    // }, {
-    //     path: '',
-    //     children: [ {
-    //         path: 'maps',
-    //         component: MapsComponent
-    //     }]
-    // }, {
-    //     path: '',
-    //     children: [ {
-    //         path: 'typography',
-    //         component: TypographyComponent
-    //     }]
-    // }, {
-    //     path: '',
-    //     children: [ {
-    //         path: 'upgrade',
-    //         component: UpgradeComponent
-    //     }]
-    // }
+    { path: 'home',     component: TableListComponent },
+
+
+
+    
     { path: 'dashboard',      component: DashboardComponent },
     { path: 'user-profile',   component: UserProfileComponent },
-    { path: 'home',     component: TableListComponent },
     { path: 'typography',     component: TypographyComponent },
     { path: 'icons',          component: IconsComponent },
     { path: 'maps',           component: MapsComponent },
